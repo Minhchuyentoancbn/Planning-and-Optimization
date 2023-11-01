bb35001a45715c17a4c570ac7b5e0b119025f030
Le Duc Minh <minhmeo7a1@gmail.com>
2023-11-01 10:01:15 +0700

    Add backtracking

--- backtrack.py
+++ backtrack.py
@@ -1,6 +1,14 @@
 # Backtracking algorithm for scheduling exams
+best_num_days = 1000000000
+best_schedule_class = None
+best_schedule_slot = None
+schedule_class = None
+schedule_slot = None
 
 def read_data():
+    """
+    Read data from stdin
+    """
     n, m = list(map(int, input().split()))
     d = list(map(int, input().split()))  # Number of students per class
     assert len(d) == n, "Number of classes does not match"
@@ -13,6 +21,7 @@ def read_data():
 
     return n, m, d, c, conflicts
 
+
 def build_conflict_table(n, conflicts):
     conflict_table = [[0 for _ in range(n)] for _ in range(n)]
     for i, j in conflicts:
@@ -24,22 +33,12 @@ def build_conflict_table(n, conflicts):
 
 
 def solution():
-    global best_num_days
+    global best_num_days, current_schedule_day
     # Compute number of days needed for this solution
     # For all assigned that use slot 1, compute the number of days
-    room_slot_assigned = [[0 for _ in range(4)] for _ in range(m)]
-    for i in range(n):
-        room = schedule_class[i]
-        slot = schedule_slot[i]
-        room_slot_assigned[room][slot - 1] += 1
 
-    schedule_day = max([max(room_slot_assigned[i]) for i in range(m)])
-
-
-    # schedule_day = max(num_days_slot)
-
-    if best_num_days > schedule_day:
-        best_num_days = schedule_day
+    if best_num_days > current_schedule_day:
+        best_num_days = current_schedule_day
         # Copy solution
         for i in range(n):
             best_schedule_class[i] = schedule_class[i]
@@ -47,7 +46,14 @@ def solution():
 
 
 def is_feasible(exam_index, room, slot):
-    # Check if exam_index can be assigned to room and slot
+    """
+    Check if exam_index can be assigned to room and slot
+    """
+    global best_num_days, room_slot_assigned
+    # Branch and bound on number of days
+    if room_slot_assigned[room][slot - 1] + 1 >= best_num_days:
+        return False
+
     # Check if there is a conflict
     for i in range(exam_index):
         if conflict_table[exam_index][i] == 1 and schedule_slot[i] == slot:
@@ -57,8 +63,9 @@ def is_feasible(exam_index, room, slot):
 
 
 def try_schedule(exam_index):
-    # print(f"Exam index: {exam_index}")
+    global best_num_days, current_schedule_day
     for room in range(m):
+        # Check capacity
         if d[exam_index] > c[room]:
             continue
         for slot in range(1, 5):
@@ -66,10 +73,17 @@ def try_schedule(exam_index):
                 continue
             schedule_class[exam_index] = room
             schedule_slot[exam_index] = slot
+            room_slot_assigned[room][slot - 1] += 1
+            old_schedule_day = current_schedule_day
+            current_schedule_day = max(current_schedule_day, room_slot_assigned[room][slot - 1])
             if exam_index == n - 1:
                 solution()
             else:
                 try_schedule(exam_index + 1)
+
+            # Backtrack
+            room_slot_assigned[room][slot - 1] -= 1
+            current_schedule_day = old_schedule_day
     return
 
                       
@@ -77,22 +91,16 @@ def try_schedule(exam_index):
 if __name__ == "__main__":
     n, m, d, c, conflicts = read_data()
     conflict_table = build_conflict_table(n, conflicts)
-    best_num_days = n
     best_schedule_class = [None for _ in range(n)]  # Class assigned to each exam
     best_schedule_slot = [None for _ in range(n)]   # Time slot assigned to each exam, 1, 2, 3, 4
 
     schedule_class = [None for _ in range(n)]  # Class assigned to each exam
     schedule_slot = [None for _ in range(n)]   # Time slot assigned to each exam, 1, 2, 3, 4
 
+    room_slot_assigned = [[0 for _ in range(4)] for _ in range(m)]
+    current_schedule_day = 0
+
     try_schedule(0)
     # Print solution
-    for i in range(1, n + 1):
-        print(f'{i} {best_schedule_slot[i]} { best_schedule_class[i] + 1}')
-
-
-
-
-
-
-
-
+    for i in range(n):
+        print(f'{i + 1} {best_schedule_slot[i]} { best_schedule_class[i] + 1}')
